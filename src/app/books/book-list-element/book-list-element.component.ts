294ee07e3f0f23080197d0a0b9aede3d975bcd68
vinczedani <dvincze@emarsys.com>
2017-12-02 10:29:31 +0100

    feat(book-list): pass down book to bookListElementComponent

--- src/app/books/book-list-element/book-list-element.component.ts
+++ src/app/books/book-list-element/book-list-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-list-element',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list-element.component.css'],
 })
 export class BookListElementComponent implements OnInit {
+  @Input() book: Book;
 
   constructor() { }
 
